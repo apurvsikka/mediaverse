f2904df3c856f8a958b7bc2201ba13cfb0cbf848
apurvsikka <apurvsikka@gmail.com>
2025-02-01 21:24:29 +0530

    fix: corrected  typos in test strings for MANGA.KAKALOT + Added READ function to it

--- test/providers/kakalot.test.ts
+++ test/providers/kakalot.test.ts
@@ -5,7 +5,7 @@ import { sleep } from "bun";
 const kk = new MANGA().KAKALOT;
 
 test("returns a filled array of search results", async () => {
-  console.log(`running test "getSearch()" expexting search results`);
+  console.log(`running test "getSearch()" expecting search results`);
   console.log("the following data has been inferred: ");
   const data = await kk.getSearch("one piece");
   expect(await data.results).not.toEqual([]);
@@ -13,7 +13,7 @@ test("returns a filled array of search results", async () => {
 });
 
 test("returns a filled array of top manga", async () => {
-  console.log(`running test "getTopManga()" expexting search results`);
+  console.log(`running test "getTopManga()" expecting search results`);
   console.log("the following data has been inferred: ");
   const data = await kk.getTopManga();
   expect(data).not.toEqual([]);
@@ -21,7 +21,7 @@ test("returns a filled array of top manga", async () => {
 });
 
 test("returns a filled array of recent manga releases", async () => {
-  console.log(`running test "getTopManga()" expexting search results`);
+  console.log(`running test "getTopManga()" expecting search results`);
   console.log("the following data has been inferred: ");
   const data = await kk.getRecentManga();
   expect(data).not.toEqual([]);
@@ -29,9 +29,27 @@ test("returns a filled array of recent manga releases", async () => {
 });
 
 test("returns information on a given manga(by id)", async () => {
-  console.log(`running test "getTopManga()" expexting search results`);
+  console.log(`running test "getTopManga()" expecting search results`);
   console.log("the following data has been inferred: ");
   const data = await kk.getMangaInfo("manga-cn1005496");
   expect(data).not.toEqual([]);
   await kk.getMangaInfo("manga-cn1005496").then((data) => console.log(data));
+});
+
+test("returns manga found under same genre ", async () => {
+  console.log(`running test "getSearch()" expecting search results`);
+  console.log("the following data has been inferred: ");
+  const data = await kk.getMangaListByGenre("shounen");
+  expect(await data.results).not.toEqual([]);
+  await kk.getMangaListByGenre("shounen").then((data) => console.log(data));
+});
+
+test("returns pages of a given chapter(by id)", async () => {
+  console.log(`running test "getPage()" expecting search results`);
+  console.log("the following data has been inferred: ");
+  const data = await kk.getPageByChapterID("manga-cn1005496", 1);
+  expect(data).not.toEqual([]);
+  await kk
+    .getPageByChapterID("manga-cn1005496", 1)
+    .then((data) => console.log(data));
 });
